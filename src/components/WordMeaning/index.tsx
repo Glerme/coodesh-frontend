326b5daef883542bb06ed4a2a11792cfaea447e8
Gui <guiggff@gmail.com>
2022-06-22 16:32:08 -0300

    :sparkles: Adicionado card

--- src/components/WordMeaning/index.tsx
+++ src/components/WordMeaning/index.tsx
@@ -1,5 +1,6 @@
-import { Meanings } from "types/Meaning";
+import type { Meanings } from "types/Meaning";
 
+import styles from "./styles.module.scss";
 interface WordMeaningProps {
   meaning: Meanings[];
 }
@@ -8,10 +9,10 @@ export const WordMeaning: React.FC<WordMeaningProps> = ({ meaning }) => {
   return (
     <div>
       {meaning?.map((meaning, i) => (
-        <div key={i}>
-          <p>{meaning?.partOfSpeech} - </p>
-          <p>{meaning?.antonyms.map((antonym) => antonym)} -</p>
-          <p>{meaning?.synonyms.map((synonym) => synonym)} -</p>
+        <div key={i} className={styles[""]}>
+          <p>{meaning?.partOfSpeech ? `${meaning.partOfSpeech},` : "-"}</p>
+          <p>{meaning?.antonyms.map((antonym) => antonym)} </p>
+          <p>{meaning?.synonyms.map((synonym) => synonym)} </p>
           <p>
             {meaning?.definitions.map((definition) => definition.definition)}
           </p>
